6ccf696c4d36f3990b05c2be98f0ca70163b84f9
Shubham Gupta <shubhamgupta25102005@gmail.com>
2025-07-21 16:42:43 +0530

    Update CPU_tb.v

--- CPU_tb.v
+++ CPU_tb.v
@@ -1,14 +1,25 @@
-
 module CPU_tb;
     reg clk;
     wire [7:0] result;
     wire zero_flag;
-    CPU cpu_inst(.clk(clk), .result(result), .zero_flag(zero_flag));
-    always #5 clk = ~clk;
+    wire [3:0] pc_out;
+
+    CPU cpu_inst(
+        .clk(clk),
+        .result(result),
+        .zero_flag(zero_flag),
+        .pc_out(pc_out)
+    );
+
     initial begin
         $dumpfile("cpu_pipeline.vcd");
         $dumpvars(0, CPU_tb);
         clk = 0;
-        #100 $stop;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        #100; // Run for some cycles
+        $stop;
     end
 endmodule
